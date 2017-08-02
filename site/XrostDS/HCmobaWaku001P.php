ecd5cd4641932d51614fa231fa9eaee972663f01
OPT-SYS\y.ashida <y.ashida@platform-id.jp>
2017-08-02 15:21:32 +0900

    ftpのファイルをアップロード

---
+++ site/XrostDS/HCmobaWaku001P.php
@@ -0,0 +1,40 @@
+﻿<?php include "./dsmodule.php"; ?>
+
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />
+
+<meta http-equiv="Content-Style-Type" content="text/css" />
+
+</head>
+
+
+
+
+<body>
+
+<hr>
+
+mobile test<br>
+
+<hr>
+
+
+
+
+<?php print(adpdsm_ad("adpds_site=hc-ishimori&adpds_frame=HC_Frame003")); ?>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ site/XrostDS/HCmobaWaku005.php
@@ -0,0 +1,42 @@
+﻿<?php include "./dsmodule.php"; ?>
+
+<html>
+
+<head>
+
+<meta http-equiv="Refresh" content="5">
+
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />
+
+<meta http-equiv="Content-Style-Type" content="text/css" />
+
+</head>
+
+
+
+
+<body>
+
+<hr>
+
+mobile test<br>
+
+<hr>
+
+
+
+
+<?php print(adpdsm_ad("adpds_site=hc-ishimori&adpds_frame=HC_MBFrame005")); ?>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
