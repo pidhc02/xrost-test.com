f4ae93b24d2e3a9a042272bb8869e0be08e92439
OPT-SYS\y.ashida <y.ashida@platform-id.jp>
2017-08-02 15:21:32 +0900

    コミット (+1 squashed commit) Squashed commits: [ecd5cd4] ftpのファイルをアップロード

---
+++ site/XrostDS/HCmobaWaku001.php
@@ -0,0 +1,35 @@
+﻿<?php include "./dsmodule.php"; ?>
+
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />
+
+<meta http-equiv="Content-Style-Type" content="text/css" />
+
+</head>
+
+
+
+
+<body>
+
+<hr>
+
+mobile test<br>
+
+<hr>
+
+
+<h1>PHP200x200</h1>
+<?php print(adpdsm_ad("adpds_site=hc-ishimori&adpds_frame=HC_MBFrame001")); ?>
+
+<h1>PHP728x90</h1>
+<?php print(adpdsm_ad("adpds_site=hc-ishimori&adpds_frame=HC_MBFrame006")); ?>
+
+
+
+</body>
+
+</html>
